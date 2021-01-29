7fcb8b062580407e5371ad72a569d2b34b0e041a
fynduck <stasskrim_-92@mail.ru>
2021-01-29 18:32:26 +0200

    updated video url parse with construct

--- src/VideoUrlParse.php
+++ src/VideoUrlParse.php
@@ -8,57 +8,74 @@
 
 namespace Fynduck\VideoUrlParser;
 
+use Illuminate\Support\Str;
 
 class VideoUrlParse
 {
+    protected $url;
+
+    protected $video_src;
+
+    public static function url(string $url): VideoUrlParse
+    {
+        return new static($url);
+    }
+
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
 
     /**
-     * validation url
-     * @param string $url
-     * @return boolean
+     * Validation url
      */
-    public static function isValidURL($url)
+    public function isValidURL(): bool
     {
-        $result = preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
-        if ($result)
-            $result = self::checkIsUrlCorrect($url);
+        $result = preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $this->url);
 
-        return $result;
+        if (!$result) {
+            return false;
+        }
+
+        switch ($this->url) {
+            case Str::contains($this->url, 'rutube.ru'):
+                return $this->isCorrectRutube();
+            case Str::contains($this->url, 'youtube.com'):
+            case Str::contains($this->url, 'youtu.be'):
+                return $this->isCorrectYoutube();
+            case Str::contains($this->url, 'vimeo.com'):
+                return $this->isCorrectVimeo();
+        }
+
+        return false;
     }
 
     /**
      * validation video url: Youtube & Rutube & Vimeo
-     * @param string $url
-     * @return boolean
+     * @return bool
      */
-    public static function checkIsUrlCorrect($url)
+    protected function checkIsUrlCorrect(): bool
     {
-        switch ($url) {
-            case str_contains($url, 'rutube.ru'):
-                $result = self::isCorrectRutube($url);
-                break;
-            case str_contains($url, 'youtube.com'):
-            case str_contains($url, 'youtu.be'):
-                $result = self::isCorrectYoutube($url);
-                break;
-            case str_contains($url, 'vimeo.com'):
-                $result = self::isCorrectVimeo($url);
-                break;
-            default:
-                $result = false;
+        switch ($this->url) {
+            case Str::contains($this->url, 'rutube.ru'):
+                return $this->isCorrectRutube();
+            case Str::contains($this->url, 'youtube.com'):
+            case Str::contains($this->url, 'youtu.be'):
+                return $this->isCorrectYoutube();
+            case Str::contains($this->url, 'vimeo.com'):
+                return $this->isCorrectVimeo();
         }
 
-        return $result;
+        return false;
     }
 
     /**
      * Check if is rutube link
-     * @param $url
      * @return bool
      */
-    protected static function isCorrectRutube($url)
+    protected function isCorrectRutube(): bool
     {
-        $urlParams = array_filter(explode('/', $url));
+        $urlParams = array_filter(explode('/', $this->url));
 
         if (isset($urlParams[3]) && $urlParams[3] == 'video' && (isset($urlParams[4]) && $urlParams[4]))
             return 'rutube';
@@ -70,12 +87,11 @@ class VideoUrlParse
 
     /**
      * Check if youtube link
-     * @param $url
      * @return bool
      */
-    protected static function isCorrectYoutube($url)
+    protected function isCorrectYoutube(): bool
     {
-        $urlParams = array_filter(explode('/', $url));
+        $urlParams = array_filter(explode('/', $this->url));
         if (isset($urlParams[3])) {
             $checkWatch = explode('?', $urlParams[3]);
             if ($urlParams[3] == 'embed' && isset($urlParams[4]) && $urlParams[4])
@@ -91,12 +107,11 @@ class VideoUrlParse
 
     /**
      * Check  if is vimeo link
-     * @param $url
      * @return bool
      */
-    protected static function isCorrectVimeo($url)
+    protected function isCorrectVimeo(): bool
     {
-        $urlParams = array_filter(explode('/', $url));
+        $urlParams = array_filter(explode('/', $this->url));
 
         if (isset($urlParams[2]) && $urlParams[2] == 'vimeo.com' && isset($urlParams[3]) && $urlParams[3])
             return 'vimeo';
@@ -108,84 +123,84 @@ class VideoUrlParse
 
     /**
      * src for embed
-     * @param string $url
      * @param bool $domain
      * @return string
      */
-    public static function returnSrcForEmbed($url, $domain = false)
+    public function returnSrcForEmbed($domain = false)
     {
         if (!$domain) {
-            $correctUrl = self::checkIsUrlCorrect($url);
+            $correctUrl = $this->checkIsUrlCorrect();
 
             if ($correctUrl)
-                return self::returnSrcForEmbed($url, $correctUrl);
+                return $this->returnSrcForEmbed($correctUrl);
         }
 
         switch ($domain) {
             case 'youtube':
-                $src = self::youtubeSrc($url);
+                $this->youtubeSrc();
                 break;
             case 'rutube':
-                $src = self::rutubeSrc($url);
+                $this->rutubeSrc();
                 break;
             case 'vimeo':
-                $src = self::vimeoSrc($url);
+                $this->vimeoSrc();
                 break;
             default:
-                $src = $url;
+                $this->video_src = $this->url;
         }
 
-        return $src;
+        return $this->video_src;
     }
 
     /**
      * Get youtube embed src
-     * @param $url
-     * @return string
+     * @return VideoUrlParse
      */
-    protected static function youtubeSrc($url)
+    protected function youtubeSrc(): VideoUrlParse
     {
         $getParams = [];
-        parse_str(parse_url($url, PHP_URL_QUERY), $getParams);
-        $checkEmbed = array_filter(explode('/', $url));
+        parse_str(parse_url($this->url, PHP_URL_QUERY), $getParams);
+        $checkEmbed = array_filter(explode('/', $this->url));
+        if ($getParams && isset($getParams['v']) && $getParams['v']) {
+            $this->video_src = 'https://www.youtube.com/embed/' . $getParams['v'];
+        } elseif (isset($checkEmbed[3]) && $checkEmbed[3] == 'embed' && isset($checkEmbed[4]) && $checkEmbed[4]) {
+            $this->video_src = $this->url;
+        }
 
-        if ($getParams && isset($getParams['v']) && $getParams['v'])
-            return 'https://www.youtube.com/embed/' . $getParams['v'];
-        elseif (isset($checkEmbed[3]) && $checkEmbed[3] == 'embed' && isset($checkEmbed[4]) && $checkEmbed[4])
-            return $url;
-
+        return $this;
     }
 
     /**
      * Get youtube embed src
-     * @param $url
-     * @return string
+     * @return VideoUrlParse
      */
-    protected static function rutubeSrc($url)
+    protected function rutubeSrc(): VideoUrlParse
     {
-        $checkEmbed = array_filter(explode('/', $url));
+        $checkEmbed = array_filter(explode('/', $this->url));
 
-        if (isset($checkEmbed[3]) && $checkEmbed[3] == 'video' && (isset($checkEmbed[4]) && $checkEmbed[4]))
-            return 'https://rutube.ru/play/embed/' . $checkEmbed[4];
-        elseif (isset($checkEmbed[4]) && $checkEmbed[4] == 'embed' && isset($checkEmbed[5]) && $checkEmbed[5])
-            return $url;
+        if (isset($checkEmbed[3]) && $checkEmbed[3] == 'video' && (isset($checkEmbed[4]) && $checkEmbed[4])) {
+            $this->video_src = 'https://rutube.ru/play/embed/' . $checkEmbed[4];
+        } elseif (isset($checkEmbed[4]) && $checkEmbed[4] == 'embed' && isset($checkEmbed[5]) && $checkEmbed[5]) {
+            $this->video_src = $this->url;
+        }
 
+        return $this;
     }
 
     /**
      * Get vimeo embed src
-     * @param $url
-     * @return string
+     * @return VideoUrlParse
      */
-    protected static function vimeoSrc($url)
+    protected function vimeoSrc(): VideoUrlParse
     {
-        $checkEmbed = array_filter(explode('/', $url));
+        $checkEmbed = array_filter(explode('/', $this->url));
 
-        if (isset($checkEmbed[2]) && $checkEmbed[2] == 'vimeo.com' && (isset($checkEmbed[3]) && $checkEmbed[3]))
-            return 'https://player.vimeo.com/video/' . $checkEmbed[3];
-        elseif (isset($checkEmbed[3]) && $checkEmbed[3] == 'video' && isset($checkEmbed[4]) && $checkEmbed[4])
-            return $url;
+        if (isset($checkEmbed[2]) && $checkEmbed[2] == 'vimeo.com' && (isset($checkEmbed[3]) && $checkEmbed[3])) {
+            $this->video_src = 'https://player.vimeo.com/video/' . $checkEmbed[3];
+        } elseif (isset($checkEmbed[3]) && $checkEmbed[3] == 'video' && isset($checkEmbed[4]) && $checkEmbed[4]) {
+            $this->video_src = $this->url;
+        }
 
+        return $this;
     }
-
 }
